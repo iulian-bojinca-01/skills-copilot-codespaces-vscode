9808789375b31b54e4f9c5e209ef021a4ece3368
iulian-bojinca-01 <168253718+iulian-bojinca-01@users.noreply.github.com>
2024-05-01 05:51:05 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+  return {
+    name: 'skillsMember',
+    type: 'member',
+    member: {
+      name: 'skills',
+      type: 'array',
+      array: {
+        type: 'string'
+      }
+    }
+  }
+}
